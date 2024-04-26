f29c056c105f8d7d61a24bd9fe674478ecb81af9
Guillaume Crégut <gcregut@free.fr>
2024-04-26 23:33:55 +0200

    [Feat]: User: modify values OK

---
+++ src/Controller/UserController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Services\PictureConvert;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/user', name: 'user_')]
+class UserController extends AbstractController
+{
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig');
+    }
+    #[Route('/edit', name: 'edit')]
+    public function edit(
+        Request $request,
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $userPasswordHasher,
+    ): Response {
+        /**
+         * @var User $user
+         */
+        $user = $this->getUser();
+        $form = $this->createForm(UserType::class, $user);
+        //Check form
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $checkPassword = $request->get('user')['checkPassword'] ?? '';
+            if ($userPasswordHasher->isPasswordValid($user, $checkPassword)) {
+                /**
+                 * @var \Symfony\Component\HttpFoundation\File\UploadedFile $avatar
+                 */
+                $avatar = $form->get('avatarUpload')->getData();
+                if ($avatar !== null && $avatar->getError() === 0) {
+                    $newAvatar = PictureConvert::convert($avatar, 100);
+                    /**
+                     * @var string $pictureDirectory
+                     */
+                    $pictureDirectory = $this->getParameter('app.dir.avatar');
+                    $fileName = $pictureDirectory . DIRECTORY_SEPARATOR .
+                        (string)$user->getId() . DIRECTORY_SEPARATOR . 'avatar.png';
+                    imagepng($newAvatar, $fileName);
+                    imagedestroy($newAvatar);
+                    $user->setAvatar('avatar.png');
+                }
+                $em->persist($user);
+                $em->flush();
+                $this->addFlash('success', 'Modifications effectuées');
+                return $this->redirectToRoute('user_index');
+            } else {
+                $field = $form->get('checkPassword');
+                $field->addError(new FormError('Mot de passe incorrect'));
+            }
+        }
+        return $this->render('user/edit.html.twig', [
+            'form' => $form
+        ]);
+    }
+}
---
+++ src/Form/UserType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Validator\Constraints\File;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('avatarUpload', FileType::class, [
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '500k',
+                        'mimeTypes' => [
+                            'image/png',
+                            'image/jpeg',
+                            'image/jpg',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez choisir un fichier jpg ou png'
+                    ])
+                ]
+            ])
+            ->add('checkPassword', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'required' => true,
+                'mapped' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
