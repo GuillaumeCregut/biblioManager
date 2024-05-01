8a924b8978bdc88fc656e9488825726bc7c644ab
Guillaume Crégut <gcregut@free.fr>
2024-05-01 15:14:48 +0200

    [Feat]: Library: create controller and index page

---
+++ src/Controller/Admin/LibraryController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Library;
+use App\Repository\LibraryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin/library', name: 'library_')]
+class LibraryController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(LibraryRepository $lib): Response
+    {
+        $libraries = $lib->findAll();
+        return $this->render('admin/library/index.html.twig', [
+            'libraries' => $libraries,
+        ]);
+    }
+
+    #[Route('/detail/{id}', name: 'detail', methods: 'GET')]
+    public function detail(Library $library): Response
+    {
+        return $this->render('admin/library/detail.html.twig', [
+            'library' => $library,
+        ]);
+    }
+    #[Route('/update/{id}', name: 'update', methods: ['GET', 'POST'])]
+    public function update(Library $library, EntityManagerInterface $em): Response
+    {
+        dd($library);
+    }
+    #[Route('/delete/{id}', name: 'delete', methods: ['POST', 'DELETE'])]
+    public function delete(Library $library, EntityManagerInterface $em): Response
+    {
+        dd($library);
+    }
+}
