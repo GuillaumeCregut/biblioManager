ffa4221a1a16dea17276198fbde9c044dbe33b75
Guillaume Crégut <gcregut@free.fr>
2024-05-02 12:17:16 +0200

    [Feat]: Entities: add constraints

--- src/Entity/Library.php
+++ src/Entity/Library.php
@@ -6,6 +6,7 @@ use App\Repository\LibraryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: LibraryRepository::class)]
 class Library
@@ -16,6 +17,7 @@ class Library
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $name = null;
 
     /**
--- src/Entity/Shelf.php
+++ src/Entity/Shelf.php
@@ -6,6 +6,7 @@ use App\Repository\ShelfRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ShelfRepository::class)]
 class Shelf
@@ -16,6 +17,7 @@ class Shelf
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $name = null;
 
     #[ORM\ManyToOne(inversedBy: 'shelves')]
